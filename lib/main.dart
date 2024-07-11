b8c224d29f36713433df5807821559c1323b3761
Aayush Shrestha <Aayushshrestha2003@gmail.com>
2024-07-11 17:14:25 +0545

    completed setup and configuration to use firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/game_provider.dart';
 
+//firebase stuff
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  //firebase initialization
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
