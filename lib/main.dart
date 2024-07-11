12c86c2ffa5fe6625f2400c3f05d744e29f5b1ae
Aayush Shrestha <Aayushshrestha2003@gmail.com>
2024-07-11 17:40:30 +0545

    updated main to use fireStoreController as provider supplier as well

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:connect4/multiplayer/firestore_controller.dart';
 import 'package:connect4/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -6,7 +7,6 @@ import 'providers/game_provider.dart';
 // Firebase imports
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -20,7 +20,14 @@ Future<void> main() async {
     MultiProvider(
       providers: [
         Provider<FirebaseFirestore>.value(value: FirebaseFirestore.instance),
-        ChangeNotifierProvider(create: (context) => GameProvider()),
+        Provider<FirestoreService>(
+          create: (context) => FirestoreService(FirebaseFirestore.instance),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => GameProvider(
+            Provider.of<FirestoreService>(context, listen: false),
+          ),
+        ),
       ],
       child: const MyApp(),
     ),
